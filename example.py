6574a956e8065951d457175fc2705ee1298dd135
codesharedot <54853330+codesharedot@users.noreply.github.com>
2019-09-12 12:30:15 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,20 +1,21 @@
-import requests
-import json
-from forex_python.converter import CurrencyRates
-import os
-
-c = CurrencyRates()
-rate = c.get_rate('USD', 'EUR') 
-print(rate)
-
-bitcoin-sv_api_url = 'https://api.coinmarketcap.com/v1/ticker/bitcoin-sv/'
-response = requests.get(bitcoin-sv_api_url)
-response_json = response.json()
-print(response_json)
-
-for coin in response.json():
-    price = coin.get("price_usd", "U$S Price not provided")
-    bitcoin-sv_price = float(("{0:.2f}").format(float(price)))
-    print("$ " + str(bitcoin-sv_price))
-    bitcoin-sv_price_eur = float(("{0:.2f}").format(float(price)*rate))   
-    print("€ " + str(bitcoin-sv_price_eur))
+import requests                                                                 
+import json                                                                     
+from forex_python.converter import CurrencyRates                                
+import os                                                                       
+                                                                                
+c = CurrencyRates()                                                             
+rate = c.get_rate('USD', 'EUR')                                                 
+print(rate)                                                                     
+                                                                                
+bitcoin_sv_api_url = 'https://api.coinmarketcap.com/v1/ticker/bitcoin-sv/'      
+response = requests.get(bitcoin_sv_api_url)                                     
+response_json = response.json()                                                 
+print(response_json)                                                            
+                                                                                
+for coin in response.json():                                                    
+    price = coin.get("price_usd", "U$S Price not provided")                     
+    bitcoin_sv_price = float(("{0:.2f}").format(float(price)))                  
+    print("$ " + str(bitcoin_sv_price))                                         
+    bitcoin_sv_price_eur = float(("{0:.2f}").format(float(price)*rate))         
+    print("€ " + str(bitcoin_sv_price_eur))                                     
+                                                       
